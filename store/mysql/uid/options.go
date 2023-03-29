8f7a0b64f3966d247b950de83afa200155d3d561
Tarsier <tarsier.t99@gmail.com>
2023-03-29 15:00:01 +0800

    uid updaet

--- store/mysql/uid/options.go
+++ store/mysql/uid/options.go
@@ -1,1 +1,64 @@
 package uid
+
+import "gorm.io/gorm"
+
+// 日志接口
+type Log interface {
+	Error(erargs ...interface{})
+}
+
+type CheckFunc func(num ...uint32) uint32
+
+type Options struct {
+	table  string //表名称
+	db     *gorm.DB
+	logger Log
+	id     string //业务id
+	len    uint32
+
+	fun []CheckFunc
+}
+
+type Option func(o *Options)
+
+func Logger(c Log) Option {
+	return func(o *Options) {
+		o.logger = c
+	}
+}
+
+func DB(c *gorm.DB) Option {
+	return func(o *Options) {
+		o.db = c
+	}
+}
+func Len(c uint32) Option {
+	return func(o *Options) {
+		o.len = c
+	}
+}
+func ID(c string) Option {
+	return func(o *Options) {
+		o.id = c
+	}
+}
+func Table(c string) Option {
+	return func(o *Options) {
+		o.table = c
+	}
+}
+
+func Fun(c []CheckFunc) Option {
+	return func(o *Options) {
+		o.fun = append(o.fun, c...)
+	}
+}
+
+func DefaultOptions() Options {
+	o := Options{
+		table: "uid",
+		id:    "user_auth",
+		len:   100,
+	}
+	return o
+}
--- store/mysql/uid/uid.go
+++ store/mysql/uid/uid.go
@@ -2,130 +2,118 @@ package uid
 
 import (
 	"errors"
-	"log"
 	"time"
-
-	"gorm.io/gorm"
 )
 
-type logger interface {
-	Error(error)
-}
-
 // Logger Log接口，如果设置了Logger，就使用Logger打印日志，如果没有设置，就使用内置库log打印日志
-var Logger logger
+//var Logger logger
 
 // ErrTimeOut 获取uid超时错误
 var ErrTimeOut = errors.New("get uid timeout")
 
 // UID struct
 type UID struct {
-	db         *gorm.DB    // 数据库连接
-	businessID string      // 业务id
-	ch         chan uint32 // id缓冲池
+	ch chan uint32 // id缓冲池
 
-	min, max  uint32 // id段最小值，最大值
-	checkFunc []CheckFunc
+	min, max uint32 // id段最小值，最大值
+
+	opt Options
 }
-
-type CheckFunc func(num ...uint32) uint32
 
 // New 创建一个UID;len：缓冲池大小()
 // db:数据库连接
-// businessID：业务id
 // len：缓冲池大小(长度可控制缓存中剩下多少id时，去DB中加载)
-func New(db *gorm.DB, businessID string, len int) (*UID, error) {
+func New(opts ...Option) (*UID, error) {
+
+	opt := DefaultOptions()
+	for _, o := range opts {
+		o(&opt)
+	}
+
 	lid := UID{
-		db:         db,
-		businessID: businessID,
-		ch:         make(chan uint32, len),
+		ch:  make(chan uint32, opt.len),
+		opt: opt,
 	}
 	go lid.productID()
 	return &lid, nil
 }
 
 // Get 获取自增id,当发生超时，返回错误，避免大量请求阻塞，服务器崩溃
-func (u *UID) Get() (uint32, error) {
+func (e *UID) Get() (uint32, error) {
 	select {
 	case <-time.After(1 * time.Second):
 		return 0, ErrTimeOut
-	case uid := <-u.ch:
+	case uid := <-e.ch:
 		return uid, nil
 	}
 }
 
 // productID 生产id，当ch达到最大容量时，这个方法会阻塞，直到ch中的id被消费
-func (u *UID) productID() {
-	u.reLoad()
+func (e *UID) productID() {
+	e.reLoad()
 	for {
-		if u.min >= u.max {
-			u.reLoad()
+		if e.min >= e.max {
+			e.reLoad()
 		}
-		u.min++
+		e.min++
 		//过滤方法
-		if len(u.checkFunc) > 0 {
-			filter := u.checkFunc[0]
-			if filter(u.min) != 0 {
-				u.ch <- u.min
+		if len(e.opt.fun) > 0 {
+			filter := e.opt.fun[0]
+			if filter(e.min) != 0 {
+				e.ch <- e.min
 			}
 		} else {
-			u.ch <- u.min
+			e.ch <- e.min
 		}
 
 	}
 }
 
 // reLoad 在数据库获取id段，如果失败，会每隔一秒尝试一次
-func (u *UID) reLoad() error {
+func (e *UID) reLoad() error {
 	var err error
 	for {
-		err = u.getFromDB()
+		err = e.getFromDB()
 		if err == nil {
 			return nil
 		}
 
 		// 数据库发生异常，等待一秒之后再次进行尝试
-		if Logger != nil {
-			Logger.Error(err)
-		} else {
-			log.Println(err)
+		if e.opt.logger != nil {
+			e.opt.logger.Error(err)
 		}
 		time.Sleep(time.Second)
 	}
 }
 
 // getFromDB 从数据库获取id段
-func (u *UID) getFromDB() error {
+func (e *UID) getFromDB() error {
 	type result struct {
 		MaxID uint32
 		Step  uint32
 	}
 	var rs result
 
-	tx := u.db.Begin()
+	tx := e.opt.db.Begin()
 	defer tx.Rollback()
 
-	err := tx.Raw("SELECT max_id,step FROM uid WHERE business_id = ? FOR UPDATE", u.businessID).Scan(&rs).Error
-	// err := row.Scan(&maxID, &step)
-	if err != nil {
+	if err := tx.Raw("SELECT max_id,step FROM ? WHERE business_id = ? FOR UPDATE", e.opt.table, e.opt.id).Scan(&rs).Error; err != nil {
 		return err
 	}
 	//步长过滤。避免productID多次调用db执行
-	if len(u.checkFunc) > 1 {
-		filter := u.checkFunc[1]
+	if len(e.opt.fun) > 1 {
+		filter := e.opt.fun[1]
 		rs.MaxID = filter(rs.MaxID, rs.Step)
 	}
 
-	err = tx.Exec("UPDATE uid SET max_id = ? WHERE business_id = ?", rs.MaxID+rs.Step, u.businessID).Error
-	if err != nil {
+	if err := tx.Exec("UPDATE ? SET max_id = ? WHERE business_id = ?", e.opt.table, rs.MaxID+rs.Step, e.opt.id).Error; err != nil {
 		return err
 	}
-	err = tx.Commit().Error
-	if err != nil {
+	if err := tx.Commit().Error; err != nil {
 		return err
 	}
 
-	u.min = rs.MaxID
-	u.max = rs.MaxID + rs.Step
+	e.min = rs.MaxID
+	e.max = rs.MaxID + rs.Step
 	return nil
 }
