399f3b9f6e2b68a3284b85dbccbda55605346660
Tarsier <tarsier.t99@gmail.com>
2025-06-21 00:24:20 +0800

    cros

--- api/cors.go
+++ api/cors.go
@@ -43,6 +43,27 @@ func Cros(w http.ResponseWriter, req *http.Request, headers ...map[string]string
 		}
 	}
 
+	/**
+	 * 更多时候推荐直接nginx去处理
+	 * 如果这里和nginx有冲突。可以按照如下方案处理
+		proxy_hide_header Access-Control-Allow-Origin;
+		proxy_hide_header Access-Control-Allow-Methods;
+		proxy_hide_header Access-Control-Allow-Headers;
+
+		add_header 'Access-Control-Allow-Origin' '*' always;
+		add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS, PUT, DELETE' always;
+		add_header 'Access-Control-Allow-Headers' 'Authorization,Content-Type,Accept,Origin,X-Requested-With,Access-Token,Access-Fp' always;
+		#这里需要cdn如果有启用http2的话会有类似cloudflare 520的问题。需要关闭掉
+		#cloudflare的处理方式-》“速度” > “优化” > “协议优化”中禁用与源站的 HTTP/2 连接。
+		if ($request_method = 'OPTIONS') {
+			add_header 'Access-Control-Allow-Origin' '*' always;
+			add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS, PUT, DELETE' always;
+			add_header 'Access-Control-Allow-Headers' 'Authorization,Content-Type,Accept,Origin,X-Requested-With,Access-Token,Access-Fp' always;
+			add_header 'Content-Length' 0;
+			add_header 'Content-Type' 'text/plain; charset=UTF-8';
+			return 200;
+		}
+	*/
 	if origin := req.Header.Get("Origin"); origin != "" {
 		w.Header().Set("Access-Control-Allow-Origin", origin)
 		if req.Method == "OPTIONS" && req.Header.Get("Access-Control-Request-Method") != "" {
